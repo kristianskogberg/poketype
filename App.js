f4c8b646501339660e71beb9f8eb30bbe4aedfe1
Kristian Skogberg <k.skogberg98@gmail.com>
2026-03-18 19:05:08 +0200

    update expo to v55

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
-//import { SafeAreaView } from 'react-native-safe-area-context'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
 import HomeScreen from './components/screens/HomeScreen'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
-import { SafeAreaView } from 'react-native'
 
 export default function App() {
   return (
